b703f226ac147494b1c580a6249593e86fd9730b
david cioato <cioatodavid@gmail.com>
2022-12-20 19:10:28 -0300

    update

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const session = require('express-session');
 const passport = require('passport');
 const cors = require('cors');
+const session = require('express-session');
 const connectDB = require('./configs/mongo.config');
 
 const authRoute = require('./routes/auth.route');
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+
+
 const dotenv = require('dotenv');
 
 dotenv.config();
